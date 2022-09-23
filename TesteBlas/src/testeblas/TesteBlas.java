07d08c6ba320935ac9dfc9c459229512a2495fc1
Tiago Gonçalves da Silva <56523236+TiagoSilva145@users.noreply.github.com>
2022-09-23 14:12:07 -0300

    Add files via upload

--- TesteBlas/src/testeblas/TesteBlas.java
+++ TesteBlas/src/testeblas/TesteBlas.java
@@ -67,6 +67,88 @@ public class TesteBlas {
         return matrix.transpose();
     }
     
+    static float calc_alfa(FloatMatrix r, FloatMatrix p)
+    {
+        FloatMatrix aux = r.transpose();
+        aux = aux.mmul(r);
+        
+        FloatMatrix aux2 = p.transpose();
+        aux2 = aux2.mmul(p);
+        
+        return aux.get(0,0) / aux2.get(0,0);
+    }
+    
+    static FloatMatrix calc_f1(FloatMatrix f, FloatMatrix p, float alfa)
+    {
+        FloatMatrix aux = p.mul(alfa);
+        return f.add(aux);
+    }
+    
+    static FloatMatrix calc_r1(FloatMatrix r, FloatMatrix modelo, FloatMatrix p, float alfa)
+    {
+        FloatMatrix aux = modelo.mmul(p);
+        aux = aux.mul(alfa);
+        return r.sub(aux);
+    }
+    
+    static float calc_beta(FloatMatrix r1, FloatMatrix r)
+    {
+        FloatMatrix aux = r1.transpose();
+        aux = aux.mmul(r1);
+        
+        FloatMatrix aux2 = r.transpose();
+        aux2 = aux2.mmul(r);
+        
+        return aux.get(0,0) / aux2.get(0,0);
+    }
+    
+    static FloatMatrix calc_p1(FloatMatrix modelo, FloatMatrix r1, FloatMatrix p, float beta)
+    {
+        FloatMatrix aux = modelo.transpose();
+        aux = aux.mmul(r1);
+        
+        FloatMatrix aux2 = p.mul(beta);
+        
+        return aux.add(aux2);
+    }
+    
+    static void cgne(String caminho_arquivo, int linhas, int colunas, int lado, int s, int n)
+    {
+        FloatMatrix modelo = le_csv(caminho_arquivo+"H-1.csv");
+        FloatMatrix modeloTrans = modelo.transpose();
+        
+        float c = (modelo.mmul(modeloTrans)).norm2();
+        
+        //pra que serve o c?
+        //g é puro ou precisa de ganho de sinal?
+        
+        FloatMatrix f = new FloatMatrix().zeros(linhas, colunas);
+        FloatMatrix r = le_csv(caminho_arquivo+"G-1.csv");
+        FloatMatrix p = modeloTrans.mmul(r);
+        
+       float erro = 1;
+       
+       while(erro > 0.0001)
+       {
+           //float alfa = ((r0.transpose()).mmul(r0)).get(0,0) / ((p0.transpose()).mmul(p1)).get(0,0);
+           float alfa = calc_alfa(r, p);
+           
+           //FloatMatrix f1 = f0.add(p0.mul(alfa));
+           FloatMatrix f1 = calc_f1(f, p, alfa);
+           
+           //FloatMatrix r1 = r0.sub((modelo.mmul(p0)).mul(alfa));
+           FloatMatrix r1 = calc_r1(r, modelo, p, alfa);
+           
+           float beta = calc_beta(r1, r);
+           
+           FloatMatrix p1 = calc_p1(modelo, r1, p, beta);
+           
+           erro = r1.norm2() - r.norm2();
+       }
+       
+        System.out.println("\n\n\nAcabou!!!");
+    }
+    
     /**
      * @param args the command line arguments
      */
@@ -132,6 +214,16 @@ public class TesteBlas {
         System.out.println("Espera-se " + le_csv(caminho+"aM.csv"));
         System.out.println("Temos:    " + a.mmul(m));
         
+        try {
+            System.out.println("");
+            System.out.println("Ma: ");
+            System.out.println("Espera-se erro");
+            System.out.println("Temos:    " + m.mmul(a));
+        } catch (Exception e) {
+            System.out.println("Erro: " + e);
+        }
+        
+        cgne("matrizModelo/modelo1/", 50816, 3600, 60, 794, 64);
     }
     
 }
